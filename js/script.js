33f77ca4bfb5242aab3e8c0f764aeb1e9b6014a9
tnphat203 <tnphat203@gmail.com>
2026-07-20 18:27:40 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,17 +1,81 @@
-const searchInput = document.getElementById("search");
+let DATA = {};
 
-searchInput.addEventListener("keyup", function () {
-  const keyword = this.value.toLowerCase();
-
-  const stores = document.querySelectorAll(".store");
-
-  stores.forEach((store) => {
-    const text = store.innerText.toLowerCase();
-
-    if (text.includes(keyword)) {
-      store.style.display = "block";
-    } else {
-      store.style.display = "none";
-    }
+fetch("./data/stores.json")
+  .then((response) => response.json())
+  .then((data) => {
+    DATA = data;
+    init();
+  })
+  .catch((error) => {
+    console.error("Không load được dữ liệu:", error);
   });
-});
+
+function init() {
+  const brandSelect = document.getElementById("brand");
+  const hospitalSelect = document.getElementById("hospital");
+  const result = document.getElementById("result");
+
+  brandSelect.addEventListener("change", () => {
+    hospitalSelect.innerHTML = '<option value="">-- Chọn bệnh viện --</option>';
+
+    result.innerHTML = "";
+
+    if (!brandSelect.value) return;
+
+    DATA[brandSelect.value].forEach((item, index) => {
+      hospitalSelect.innerHTML += `
+        <option value="${index}">
+          ${item.hospital}
+        </option>
+      `;
+    });
+  });
+
+  hospitalSelect.addEventListener("change", () => {
+    result.innerHTML = "";
+
+    if (hospitalSelect.value === "") return;
+
+    const item = DATA[brandSelect.value][hospitalSelect.value];
+
+    let html = `
+      <h3 class="result-title">
+        📍 Cửa hàng gần nhất
+      </h3>
+
+      <div class="store-card">
+
+        <h3>${item.main.name}</h3>
+
+        <p>${item.main.address}</p>
+
+        <span class="badge">
+          Khuyến nghị đổi quà tại đây
+        </span>
+
+      </div>
+    `;
+
+    if (item.optional.length) {
+      html += `
+        <div class="optional-title">
+          Hoặc có thể đổi tại
+        </div>
+      `;
+
+      item.optional.forEach((store) => {
+        html += `
+          <div class="store-card alt">
+
+            <h3>${store.name}</h3>
+
+            <p>${store.address}</p>
+
+          </div>
+        `;
+      });
+    }
+
+    result.innerHTML = html;
+  });
+}
